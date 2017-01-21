954962619c67a7920609c405c5221ea1de00be82
Panultima <BeanDerDoneDat@gmail.com>
2017-01-20 20:51:40 -0500

    PNG Simple is working

--- src/PNGSimple.java
+++ src/PNGSimple.java
@@ -2,6 +2,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,9 +18,9 @@ public class PNGSimple {
     static void writeToImage(byte[] data, String path)
     {
         try {
-            int height = (int) Math.ceil(Math.sqrt(data.length/3));
+            int height = (int) Math.ceil(Math.sqrt((data.length/3)+1));
             int width = height;
-            while(width*height>data.length/3)
+            while(width*height>(data.length/3)+1)
             {
                 width = width-1;
             }
@@ -29,6 +30,9 @@ public class PNGSimple {
 
             // Value to index the byte[];
             int bI = 0;
+            int pI = 0;
+            int paddingLength = width*height*3 - data.length;
+            byte[] padding = ByteBuffer.allocate(4).putInt(paddingLength).array();
 
             // Iterate through the pixel rows;
             for(int i = 0; i < height; i++) {
@@ -36,12 +40,26 @@ public class PNGSimple {
                 for(int j = 0; j < width; j++) {
                     // Pull out 4 bytes and generate colour int;
                     // This entire statement depends on bytesPerPixel;
-                    int pixelVal1 = bI < data.length ? Byte.toUnsignedInt(data[bI]) : 0;
-                    int pixelVal2 = bI+1 < data.length ? Byte.toUnsignedInt(data[bI+1]) : 0;
-                    int pixelVal3 = bI+2 < data.length ? Byte.toUnsignedInt(data[bI+2]) : 0;
-                    int rgb=new Color(pixelVal1,pixelVal2,pixelVal3).getRGB();
-                    image.setRGB(j,i, rgb);
-                    bI=bI+3;
+                    if(i==0 && j==0) {
+                        int p1 = Byte.toUnsignedInt(padding[1]);
+                        int p2 = Byte.toUnsignedInt(padding[2]);
+                        int p3 = Byte.toUnsignedInt(padding[3]);
+                        int rgb=new Color(p1,p2,p3).getRGB();
+                        image.setRGB(j,i,rgb);
+                    }
+                    else {
+                        int pixelVal1 = bI < data.length ? Byte.toUnsignedInt(data[bI]) : Byte.toUnsignedInt(data[pI]);
+                        int pixelVal2 = bI + 1 < data.length ? Byte.toUnsignedInt(data[bI + 1]) : Byte.toUnsignedInt(data[pI + 1]);
+                        int pixelVal3 = bI + 2 < data.length ? Byte.toUnsignedInt(data[bI + 2]) : Byte.toUnsignedInt(data[pI + 2]);
+                        int rgb = new Color(pixelVal1, pixelVal2, pixelVal3).getRGB();
+                        image.setRGB(j, i, rgb);
+                        if (bI + 2 >= data.length) {
+                            pI = pI + 3;
+                        }
+                        if (bI + 2 < data.length) {
+                            bI = bI + 3;
+                        }
+                    }
                 }
             }
 
@@ -73,8 +91,16 @@ public class PNGSimple {
                 }
             }
 
+            int length = recoveredBytes.length;
+            byte[] pad = {recoveredBytes[0], recoveredBytes[1],recoveredBytes[2]};
+            int paddinglength = new BigInteger(pad).intValue();
+
+            byte[] finalBytes = new byte[length-paddinglength];
+            System.arraycopy(recoveredBytes, 3, finalBytes, 0, length-paddinglength);
+
+
             FileOutputStream fos = new FileOutputStream(recoveredPath);
-            fos.write(recoveredBytes);
+            fos.write(finalBytes);
             fos.close();
         }catch(Exception e) {}
     }
