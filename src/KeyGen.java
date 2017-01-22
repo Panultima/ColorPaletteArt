6600a5555fa91dcf6e00e2ab955ec5cac937acfe
Panultima <BeanDerDoneDat@gmail.com>
2017-01-22 15:27:08 -0500

    Added required files

---
+++ src/KeyGen.java
@@ -0,0 +1,37 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by nathaniel on 1/22/17.
+ */
+public class KeyGen {
+
+    public static byte[] getKeyFromFile(String dir)
+    {
+        byte[] key = new byte[16];
+
+        try {
+            Path path = Paths.get(dir);
+            byte[] data = Files.readAllBytes(path);
+            key[0] = data[3];
+            key[1] = data[21];
+            key[2] = data[7];
+            key[3] = data[32];
+            key[4] = data[41];
+            key[5] = data[167];
+            key[6] = data[1022];
+            key[7] = data[0];
+            key[8] = data[2000];
+            key[9] = data[567];
+            key[10] = data[3001];
+            key[11] = data[9];
+            key[12] = data[1500];
+            key[13] = data[1501];
+            key[14] = data[2121];
+            key[15] = data[2222];
+        } catch(Exception e) {}
+
+        return key;
+    }
+}
